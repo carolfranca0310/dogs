ab335e5bf01f4a0e012a2f13796ff7d73c8cd5d5
Caroline <carolfranca0310@gmail.com>
2022-09-12 11:18:00 -0300

    criação da endpoint UserPost

--- src/api/endpoints/UserPost.js
+++ src/api/endpoints/UserPost.js
@@ -5,14 +5,52 @@ const UserPost = () => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
+function handleSubmit(event){
+  // preventDefault para não recarregar a página
+    event.preventDefault();
+    fetch('https://dpgsapi.origamid.dev/json/api/user',{
+      method:'POST',
+      headers:{
+        'Content-Type':'aplciation/json'
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    }).then(response => {
+      console.log(response);
+      return response.json();
+      
+    }).then(json => {
+      console.log(json);
+      return json;
+    });
+}
+
   return (
-    <div>
+    // onSubmit = evento de quando envio formulário
+    <form onSubmit={handleSubmit}>
        <input 
         type="text"
         placeholder="username"
         value={username}
-        onChange={({ target }) => setUsername(target.value)}/>
-    </div>
+        onChange={({ target }) => setUsername(target.value)}
+        />
+        <input 
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+        />
+        <input 
+        type="text"
+        placeholder="password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Enviar</button>
+    </form>
   )
 }
 
