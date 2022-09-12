f9f3007af963236ae1e061cb9d97cc6e9c91f955
Caroline <carolfranca0310@gmail.com>
2022-09-12 13:52:44 -0300

    criação das ultimas duas endpoints

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import TokenPost from './endpoints/TokenPost';
 import UserPost from './endpoints/UserPost';
+import PhotoGet from './endpoints/PhotoGet';
+import PhotoPost from './endpoints/PhotoPost';
 
 const Api = () => {
   return (
     <div>
-        <h2>USER POST</h2>
+        <h2>User Post</h2>
         <UserPost/>
         <h2>Token Post</h2>
         <TokenPost/>
+        <h2>Photo Post</h2>
+        <PhotoPost />
+        <h2>Photo Get</h2>
+        <PhotoGet />
     </div>
   )
 }
