ca567854f1ecf7752a9a87ce9b4098445838499d
Caroline <carolfranca0310@gmail.com>
2022-09-13 10:10:52 -0300

    header finalizado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,9 @@ const Header = () => {
           <Link className={styles.logo} to="/" aria-label="Dogs - Home">
             <Dogs/>
           </Link>
-          <Link to="/login">Login / Criar </Link>
+          <Link className={styles.login} to="/login">
+            Login / Criar 
+          </Link>
         </nav>
     </header>
   )
